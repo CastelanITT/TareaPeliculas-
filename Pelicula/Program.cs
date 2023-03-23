8aaea2ced4cbea72f5327628e426ef652b1d6f3f
José Juarez Castelán <127893647+CastelanITT@users.noreply.github.com>
2023-03-23 03:35:10 +0000

    cambiostotal

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -20,28 +20,61 @@ namespace Pelicula
         {this.titulo=titulo;  this.año=año;}
      
         //Métodos
+
+        public string GetTitulo(){return titulo;}
+        public void SetTitulo(string titu){titulo=titu;}
+
+        public int GetAño(){return año;}
+        public void SetAño(int year){año=year;}
+        
+        public string GetGenero(){return genero;}
+        public void SetGenero(string pa){genero=ge;}
+
+        public string GetDirector(){return director;}
+        public void SetDirector(string direc){director=direc;}
      
         public void Imprime()
         {
+            Console.WriteLine($"{titulo} ({año})");
      //       Console.WriteLine($"{titulo} ({año})");
-
         }
-
+            public void AgregaActor(Actor actor)
+        {
+            actores.Add(actor);
+        }
+        public void ImprimeActores()
+        {
+            foreach(Actor persona in actores)
+            {
+                Console.WriteLine($"{persona.GetNombre()} ({persona.GetAño()})");
+            }
+        }
+    
 
     }
 
     public class Actor
     {
         //Propiedades
+        string nombre;
+        int año;
+
 
         //Constructores
+        public Actor(){}
+        public Actor(string nombre, int año)
+        {this.nombre= nombre;  this.año=año;}
 
 
         //Métodos 
-        public void Imprime()
-        {
-            Console.WriteLine($"{Nombre} ({Año})");
-        }
+         public string GetNombre(){return nombre;}
+        public void SetNombre(string nomb){nombre= nomb;}
+
+        public int GetAño(){return año;}
+        public void SetAño(int añe){año=añe;}
+
+
+
     }
 
     // Puedes probar tu código en Main() pero lo importante
@@ -53,7 +86,29 @@ namespace Pelicula
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Pelicula p1=new Pelicula();
+            Pelicula p2=new Pelicula();
+
+            p1.SetTitulo("La La Land");
+            p1.SetAño(2016);
+            Console.WriteLine("{0} ({1})", p1.GetTitulo(), p1.GetAño());
+            p1.Imprime();
+
+            List<Pelicula> peliculas = new List<Pelicula>();
+            peliculas.Add(new Pelicula("Venom",2018));
+            peliculas.Add(new Pelicula("First Man",2018));
+            peliculas.Add(new Pelicula("Los Increibles",2004));
+            peliculas.Add(new Pelicula("Kimetsu No Yaiba",2023));
+            peliculas.Add(new Pelicula("Avengers Engame",2019));
+
+            foreach(Pelicula peli in peliculas)
+            {
+                peli.Imprime();
+            }
+             p1.AgregaActor(new Actor("Keanu Reeves", 1964));
+            p1.AgregaActor(new Actor("Charlize Theron", 1975));
+
+            p1.ImprimeActores();
         }
     }
 }
